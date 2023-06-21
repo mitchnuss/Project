8107935627625ec5ce41364d0005fa8c5fda53f4
Mitchell Nuss <mitch.nuss@gmail.com>
2023-06-21 13:04:33 -0500

    Refactored AdminApp for user input

--- MilestoneProject/src/app/AdminApp.java
+++ MilestoneProject/src/app/AdminApp.java
@@ -6,52 +6,66 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.Scanner;
+
 /**
  * Class to act as Client to initiate connection to server
+ * 
  * @author mitch 6/15/23
  *
  */
 public class AdminApp {
-	
+
 	private Socket clientSocket;
 	private PrintWriter out;
 	private BufferedReader in;
-	
+
 	public void start(String ip, int port) throws UnknownHostException, IOException {
 		clientSocket = new Socket(ip, port);
 		out = new PrintWriter(clientSocket.getOutputStream(), true);
 		in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 	}
-	
+
 	public String sendMessage(String command, String payload) throws IOException {
 		out.println(command);
 		out.println(payload);
-		
+
 		return in.readLine();
 	}
-	
+
 	public void cleanUp() throws IOException {
 		in.close();
 		out.close();
 		clientSocket.close();
 	}
 
-	public static void main(String[] args) throws InterruptedException, IOException{
-		
+	public static void main(String[] args) throws InterruptedException, IOException {
+
 		AdminApp client = new AdminApp();
 		client.start("127.0.0.1", 6666);
-		
-	    String retrieveCmd = "R";
-	    String retrievePyld = "";
-	    String retrieveResponse = client.sendMessage(retrieveCmd, retrievePyld);
-	    System.out.println("Retrieved Inventory: " + retrieveResponse);
-	    
-		String updateCmd = "U";
-		String updatePyld = "{\"products\": [{\"name\": \"Product1\", \"description\": \"Description1\", \"price\": 10.0, \"qty\": 5}, {\"name\": \"Product2\", \"description\": \"Description2\", \"price\": 15.0, \"qty\": 3}]}";
-	    String updateResponse = client.sendMessage(updateCmd, updatePyld);
-	    System.out.println("Updated Inventory: " + updateResponse);
-	    
-		client.cleanUp();
-	}
 
+		Scanner scnr = new Scanner(System.in);
+		boolean running = true;
+
+		while (running) {
+			System.out.println("Enter a command (U for update, R for retrieve) or 'exit' to quit:");
+			String input = scnr.nextLine();
+
+			if (input.equalsIgnoreCase("U")) {
+				System.out.println("Enter the payload for update command:");
+				String payload = scnr.nextLine();
+				String response = client.sendMessage("U", payload);
+				System.out.println("Server response:" + response);
+			} else if (input.equalsIgnoreCase("R")) {
+				String response = client.sendMessage("R", "");
+				System.out.println("Server response: " + response);
+			} else if (input.equalsIgnoreCase("exit")) {
+				running = false;
+			} else {
+				System.out.println("Invalid command.");
+			}
+		}
+		client.cleanUp();
+		scnr.close();
+	}
 }
--- MilestoneProject/src/app/Product.java
+++ MilestoneProject/src/app/Product.java
@@ -2,9 +2,7 @@ package app;
 
 public class Product <T extends SalableProduct> {
 
-	public Product(String name, String description, double price, int qty) {
-		
-		
+	public Product(String name, String description, double price, int qty) {	
 	}
 }
 	
